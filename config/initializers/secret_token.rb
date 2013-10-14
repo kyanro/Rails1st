5765b1059bb83ebf62463ffe69a7d308ecfd9d88
kyanro <gw.fnct+github-pub@gmail.com>
2013-10-14 21:35:27 +0900

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GetMorning::Application.config.secret_key_base = 'c7642f226ad269d8595bbedc3a6b4c83ce0ed45a6a0031f5fd8722ec3920440b31f26382b7badb5d242410d63d5f77799f388da379a291d767310105d59c735f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GetMorning::Application.config.session_store :cookie_store, key: '_GetMorning_session'
