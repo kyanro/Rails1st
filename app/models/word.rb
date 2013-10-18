1db9e1a26db3f711240d17b0b707e436f92cf408
kyanro <gw.fnct+github-pub@gmail.com>
2013-10-19 01:31:50 +0900

    imp 空白のpaddingをしないように修正

--- app/models/word.rb
+++ app/models/word.rb
@@ -12,7 +12,7 @@ class Word < ActiveRecord::Base
 	#= 渡された時間をもとに規定の形式の文字列型の時間を返却する
 	#	現在の既定は %-m月%-d日 %k時%M分
 	def self.toResponseTimeString(time)
-		return DateTime.parse(time).strftime("%-m月%-d日 %k時%M分")
+		return DateTime.parse(time).strftime("%-m月%-d日 %-k時%M分")
 	end
 
 	#
