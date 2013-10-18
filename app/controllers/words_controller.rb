68a2487d7c7fa9f0fcf506155ba3e8eb3de7f485
kyanro <gw.fnct+github-pub@gmail.com>
2013-10-18 21:53:40 +0900

    imp 受け取った時間を時間のみに整形して返却するところまで作成

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,11 +1,10 @@
 class WordsController < ApplicationController
 	skip_before_filter :verify_authenticity_token ,:only=>[:getmorning]
 
-
 	def index
 		@words = Word.all
 	end
 	def getmorning
-		@words = Word.all
+		@setTime = Word.getTimeFromDateTime(params[:time])
 	end
 end
