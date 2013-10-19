306139fd570aaff61c90063c688ab35ea0afba88
kyanro <gw.fnct+github-pub@gmail.com>
2013-10-19 20:43:02 +0900

    fix greater than がきいていなかったバグを修正
    sqliteは単なる文字列なので、時刻の比較の時に.000000を付ける必要があった

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,9 +3,11 @@ class Word < ActiveRecord::Base
 	#
 	#= 受け取ったDatetime文字列を "2000-01-01 %H:%M:00" のDatetime型として返却する
 	# time型の列の検索をactive record で利用するために作成
+	# sqliteの場合、単なる文字列との比較となるので、
+	# grater then を正確にきかせるために、.000000 までつける
 	#
 	def self.getTimeFromDateTimeString(time)
-		return DateTime.parse(DateTime.parse(time).strftime("2000-01-01 %H:%M"))
+		return DateTime.parse(time).strftime("2000-01-01 %H:%M:00.000000")
 	end
 
 	#
@@ -20,8 +22,8 @@ class Word < ActiveRecord::Base
 	#
 	def self.getSearchedWord(time, emotion)
 		searchedWord = 
-				 where(["since < ?", self.getTimeFromDateTimeString(time)])
-				.where(["until > ?", self.getTimeFromDateTimeString(time)])
+				 where(["since <= ?", self.getTimeFromDateTimeString(time)])
+				.where(["until >= ?", self.getTimeFromDateTimeString(time)])
 				.where(["emotion = ?", emotion])
 				.first
 
