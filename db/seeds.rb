63f9a2201b174390341ac23119701b9c5fa97b02
kyanro <gw.fnct+github-pub@gmail.com>
2013-10-20 01:12:35 +0900

    imp 初期データの投入方法を変更 それに合わせて初期データの形式も変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'csv'
+
 #
 #= Seedで追加したWordのレコードをすべて削除
 #
@@ -13,8 +15,20 @@ Word
 	.where(seed: true)
 	.delete_all
 
-# 初期データの投入
-Word.create(character_id: "1", emotion_lower_limit: 1, emotion_upper_limit: 7, word: "・・・。そんな時間に私に起きろって？正気？", since: "00:00:00", until: "04:00:00", seed: true)
-Word.create(character_id: "1", emotion_lower_limit: 1, emotion_upper_limit: 7, word: "ですね。了解しました、ご主人様。", since: "04:00:00", until: "12:00:00", seed: true)
-Word.create(character_id: "1", emotion_lower_limit: 1, emotion_upper_limit: 7, word: "ですって？一体いつまで寝るつもりなの？このダメ人間！", since: "12:00:00", until: "20:00:00", seed: true)
-Word.create(character_id: "1", emotion_lower_limit: 1, emotion_upper_limit: 7, word: "・・・。そろそろ眠り始める人がいてもおかしくない時間よ？", since: "20:00:00", until: "23:59:59", seed: true)
+#
+#= 初期データの投入
+# default.tsv の投入。encordは Excelのunicode保存にあわせて、
+# BOM付きUTF-16LE 固定とした 
+#
+options = {encoding:"BOM|UTF-16LE", col_sep: "\t", headers: :first_row}
+CSV.foreach('db/seeds/default.tsv', options){|r|
+	Word.create(
+		character_id:        r["character_id"],
+		emotion_lower_limit: r["emotion_lower_limit"],
+		emotion_upper_limit: r["emotion_upper_limit"],
+		word:                r["word"],
+		since:               r["since"],
+		until:               r["until"],
+		seed:                true
+	)
+}
